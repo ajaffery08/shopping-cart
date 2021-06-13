4d92cf31598326d95e4e978ace704aad8706fb37
Ahmed Jaffery <ajaffery08@gmail.com>
2021-06-13 15:53:42 -0400

    Cleanup
    Cleanup on .py file. Also requirements.txt did not need anything.

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,3 +1,5 @@
+#List of products based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -19,7 +21,7 @@ products = [
     {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+]
 
 
 def to_usd(my_price):
